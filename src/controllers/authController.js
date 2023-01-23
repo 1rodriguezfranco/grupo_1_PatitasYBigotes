ecd38c8978aa96b34b1bf5f8e8826225c1ed48a4
Francisco Duran <franciscoduran@MacBook-Air-de-Francisco.local>
2023-01-23 18:39:24 -0300

    Margenes actualizados en edit y create

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 const authController = {
     login: (req, res) => res.render ("login"),
     register: (req, res) => res.render ("register"),
+    register2: (req, res) => res.render ("register2"),
     newpassword: (req, res) => res.render ("newpassword"),
     recoverpassword: (req, res) => res.render ("recoverpassword"), 
 }
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,6 +5,7 @@ const authRouter = require("../controllers/authController");
 
 router.get("/login", authRouter.login);
 router.get("/register", authRouter.register);
+router.get("/register2", authRouter.register2);
 router.get("/newpassword", authRouter.newpassword);
 router.get("/recoverpassword", authRouter.recoverpassword);
 
