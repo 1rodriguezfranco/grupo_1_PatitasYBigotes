d9c278425e479d959e7a4625d9a442c272ceefcc
1rodriguezfranco <98francorodriguez@gmail.com>
2023-02-09 15:06:35 -0300

    Creando vista dinámica de lista de productos vinculada con productsData

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,8 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const mainController = {
     index: (req, res) => res.render ("index"),
     carrito: (req, res) => res.render ("carrito"),
-    productdetails: (req, res) => res.render ("productdetails"),  
+    productdetails: (req, res) => res.render ("productdetails"),
+
+    productList: (req, res) => {
+        const products = getProductList(productsFilePath);
+        res.render("productsList", {products: products})
+    }
      
 };
+
+function getProductList(path) {
+	return JSON.parse(fs.readFileSync(path, 'utf-8'));
+}
 
 module.exports = mainController;
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -5,6 +5,7 @@ const mainRouter = require("../controllers/mainController");
 router.get("/", mainRouter.index);
 router.get("/carrito", mainRouter.carrito);
 router.get("/productdetails", mainRouter.productdetails);
+router.get("/productList", mainRouter.productList)
 
 
 module.exports = router;
