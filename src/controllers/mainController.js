345ecce60c5750cfb2ea4948f1a1f0c2336c99b8
1rodriguezfranco <98francorodriguez@gmail.com>
2023-04-26 21:01:23 -0300

    Corrigiendo botón de eliminar en MOBILE, creando vista nosotros

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,7 +1,8 @@
 const mainController = {
     index: (req, res) => res.render ("index"),
     carrito: (req, res) => res.render ("carrito"),
-     
+    nosotros: (req, res) => res.render ("nosotros"),
+
 };
 
 module.exports = mainController;
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -4,6 +4,7 @@ const mainController = require("../controllers/mainController");
 
 router.get("/", mainController.index);
 router.get("/carrito", mainController.carrito);
+router.get("/nosotros", mainController.nosotros);
 
 
 module.exports = router;
