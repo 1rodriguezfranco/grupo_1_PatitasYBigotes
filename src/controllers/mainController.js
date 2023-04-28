89c2fc4f42b75807ba353842771b7cb3f76acead
1rodriguezfranco <98francorodriguez@gmail.com>
2023-04-27 23:18:07 -0300

    Agregando buscador funcional

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,8 +1,17 @@
+const path = require('path');
+const db = require(path.join(__dirname, "../../database/models"));
+
 const mainController = {
     index: (req, res) => res.render ("index"),
     carrito: (req, res) => res.render ("carrito"),
     nosotros: (req, res) => res.render ("nosotros"),
-
+    search: async (req, res) => {
+        let search = req.query.keywords;
+        let allProducts = await db.Product.findAll();
+        let productsSearched = allProducts.filter(product => product.name.toLowerCase().indexOf(search.toLowerCase()) >= 0);
+        return res.render("results", {products: productsSearched});
+        
+    }
 };
 
 module.exports = mainController;
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -5,6 +5,7 @@ const mainController = require("../controllers/mainController");
 router.get("/", mainController.index);
 router.get("/carrito", mainController.carrito);
 router.get("/nosotros", mainController.nosotros);
+router.get("/search", mainController.search);
 
 
 module.exports = router;
