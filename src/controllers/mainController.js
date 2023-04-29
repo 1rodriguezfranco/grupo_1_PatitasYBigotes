5225f69d2aa3975271ee16f70a32f33d9cf28777
1rodriguezfranco <98francorodriguez@gmail.com>
2023-04-29 18:15:26 -0300

    Subiendo el listado de productos al index

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,7 +2,10 @@ const path = require('path');
 const db = require(path.join(__dirname, "../../database/models"));
 
 const mainController = {
-    index: (req, res) => res.render ("index"),
+    index: async (req, res) => {
+        let allProducts = await db.Product.findAll(); 
+        res.render ("index", {products: allProducts});
+    },
     carrito: (req, res) => res.render ("carrito"),
     nosotros: (req, res) => res.render ("nosotros"),
     search: async (req, res) => {
